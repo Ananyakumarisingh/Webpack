be3f4413e183d2193117b0b37e679456fb81ac68
Ananyakumarisingh <ananyakumarisingh77@gmail.com>
2023-06-10 20:23:27 +0530

    while using react

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import { mult } from "./calc";
+const { add } = require("./calc")
+console.log(add(5, 6));
+console.log(mult(5, 6));
+
 // import Icon from "./icon.png";  // ! for imgs
+
 import("./index.css");
 // ! It will throw an error because webpack is taking the css code as javaScript.
 // ! Here we will use loder
 
 
-const { add } = require("./calc")
+
+// const h1 = document.createElement("h1")
+// h1.innerText = "Hello Webpack";
+// h1.classList.add("redtext");
+
+// document.getElementById('root').appendChild(h1);
 
 
-console.log(add(5, 6));
-console.log(mult(5, 6));
+// * React
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    React.createElement("h1", {className: "redtext"}, "Hello Webpack from react"),   // What 
+    document.getElementById("root")   // Where
+)
 
 
-const h1 = document.createElement("h1")
-h1.innerText = "Hello Webpack";
-h1.classList.add("redtext");
-
-document.getElementById('root').appendChild(h1);
